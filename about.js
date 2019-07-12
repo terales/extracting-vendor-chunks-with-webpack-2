ba69c1eb73132d9130c59b6433fb90d1858f993e
TerAleS <terales@users.noreply.github.com>
2019-07-12 11:05:46 +0300

    Update dependencies to resolve security alerts

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 var jquery = require('./node_modules/jquery/dist/jquery');
 var tether = require('./node_modules/tether/dist/js/tether');
-var bootstrap = require('./node_modules/bootstrap/dist/js/npm');
+var bootstrap = require('./node_modules/bootstrap/dist/js/bootstrap');
 
 console.group('All libs loaded:');
 console.log('jquery', jquery);
